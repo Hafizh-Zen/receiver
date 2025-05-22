87fd6aee97d5bd8a2f09407c969529b47c18e363
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2025-05-23 01:21:30 +0700

    Implement the receive function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -18,4 +18,12 @@ use bambangshop_receiver::Result;
          Err(e) => Err(e)
      };
 
+      }
+ #[post("/receive", data = "<notification>")]
+ pub fn receive(notification: Json<Notification>) -> Result<Json<Notification>> {
+     return match NotificationService::receive_notification(notification.into_inner()) {
+         Ok(f) => Ok(Json::from(f)),
+         Err(e) => Err(e)
+     };
+
  }
